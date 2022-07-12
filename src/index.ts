475e93a486a18540260dbfc06fc4de1814e661f9
Sinan Demirci <demirci.sinan96@gmail.com>
2022-07-12 18:14:21 +0200

    initial commit: setup backend and react client

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Helo World!')
+});
+
+const port = process.env.BACKEND_PORT;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
